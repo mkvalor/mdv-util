cfbf4352d7bea6e48afe5d7650e27626f02438a4
Mark Valor <mkvalor@gmail.com>
2016-09-02 12:08:44 -0700

    Update VM image name

--- bin/hccreate.sh
+++ bin/hccreate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcloud compute instances create homebase --image dev-ubuntu15-vivid --disk name=hc-sourcefiles-uc1b,mode=rw --machine-type=g1-small
+gcloud compute instances create homebase --image dev-ubuntu16-04-1-xerus --disk name=hc-sourcefiles-uc1b,mode=rw --machine-type=g1-small
