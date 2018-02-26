6d090bf942aff67a0d806bfcf670fcf97a16812b
Mark Valor <mkvalor@gmail.com>
2018-02-25 17:45:14 -0800

    Specify min cpu platform as Skylake

--- bin/hccreate.sh
+++ bin/hccreate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcloud compute instances create homebase --image-family ubuntu-homebase --disk name=hc-sourcefiles-uc1b,mode=rw --machine-type=f1-micro
+gcloud compute instances create homebase --image-family ubuntu-homebase --disk name=hc-sourcefiles-uc1b,mode=rw --machine-type f1-micro --min-cpu-platform "Intel Skylake"
